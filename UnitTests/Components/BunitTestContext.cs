7f2a1ed9450b17a9a1c53c210be7d7c653f4bc7d
ameh <me@ameh.io>
2021-02-07 17:09:51 -0600

    Added editor config to solution

--- UnitTests/Components/BunitTestContext.cs
+++ UnitTests/Components/BunitTestContext.cs
@@ -1,37 +1,37 @@
-﻿using System;
-using Bunit;
+﻿using Bunit;
 using Bunit.Rendering;
 using Microsoft.AspNetCore.Components;
 using NUnit.Framework;
+using System;
 
 namespace UnitTests.Components
 {
-    public abstract class BunitTestContext : IDisposable
+    public abstract class BunitTestBase : IDisposable
     {
-        private Bunit.TestContext _context;
+        private Bunit.TestContext _Context;
 
-        public ITestRenderer Renderer => _context?.Renderer ?? throw new InvalidOperationException("NUnit has not started executing tests yet");
+        public ITestRenderer Renderer => _Context?.Renderer ?? throw new InvalidOperationException("NUnit has not started executing tests yet");
 
-        public TestServiceProvider Services => _context?.Services ?? throw new InvalidOperationException("NUnit has not started executing tests yet");
+        public TestServiceProvider Services => _Context?.Services ?? throw new InvalidOperationException("NUnit has not started executing tests yet");
 
         public void Dispose()
         {
-            _context?.Dispose();
-            _context = null;
+            _Context?.Dispose();
+            _Context = null;
         }
 
         [SetUp]
-        public void Setup() => _context = new Bunit.TestContext();
+        public void Setup() => _Context = new Bunit.TestContext();
 
         [TearDown]
         public void TearDown() => Dispose();
 
-        public IRenderedComponent<TComponent> RenderComponent<TComponent>(params ComponentParameter[] parameters) 
+        public IRenderedComponent<TComponent> RenderComponent<TComponent>(params ComponentParameter[] parameters)
             where TComponent : IComponent
-            => _context?.RenderComponent<TComponent>(parameters) ?? throw new InvalidOperationException("NUnit has not started executing tests yet");
+            => _Context?.RenderComponent<TComponent>(parameters) ?? throw new InvalidOperationException("NUnit has not started executing tests yet");
 
-        public IRenderedComponent<TComponent> RenderComponent<TComponent>(Action<ComponentParameterCollectionBuilder<TComponent>> parameterBuilder) 
+        public IRenderedComponent<TComponent> RenderComponent<TComponent>(Action<ComponentParameterCollectionBuilder<TComponent>> parameterBuilder)
             where TComponent : IComponent
-            => _context?.RenderComponent<TComponent>(parameterBuilder) ?? throw new InvalidOperationException("NUnit has not started executing tests yet");
+            => _Context?.RenderComponent<TComponent>(parameterBuilder) ?? throw new InvalidOperationException("NUnit has not started executing tests yet");
     }
 }
